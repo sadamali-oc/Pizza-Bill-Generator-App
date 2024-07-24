11d6966c7bae43d33e9ff6fdd3138c4a7310ce34
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-07-24 11:05:43 +0530

    overridden method

--- PizzaBillGenerator/src/DeluxPizza.java
+++ PizzaBillGenerator/src/DeluxPizza.java
@@ -2,5 +2,17 @@ public class DeluxPizza extends  Pizza{
 
     public DeluxPizza(Boolean veg) {
         super(veg);
+        super.addExtraCheese();
+        super.addExtraToppings();
+    }
+
+    @Override
+    public void addExtraCheese() {
+
+    }
+
+    @Override
+    public void addExtraToppings() {
+
     }
 }
--- PizzaBillGenerator/src/Main.java
+++ PizzaBillGenerator/src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Pizza basePizza= new Pizza(false);
-        basePizza.addExtraCheese();
-        basePizza.addExtraToppings();
-        basePizza.takeAway();
-        basePizza.getBill();
+//        Pizza basePizza= new Pizza(false);
+//        basePizza.addExtraCheese();
+//        basePizza.addExtraToppings();
+//        basePizza.takeAway();
+//        basePizza.getBill();
+
+
+        DeluxPizza dp=new DeluxPizza(true);
+        dp.takeAway();
+        dp.getBill();
 
 
     }
