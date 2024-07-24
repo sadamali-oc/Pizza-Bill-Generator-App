a02feb75179c059173eb344c7aa1d661a48882ff
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-07-24 10:27:30 +0530

    get pizza price

--- PizzaBillGenerator/src/Main.java
+++ PizzaBillGenerator/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Pizza basePizza= new Pizza(false
+        );
+        basePizza.getPizzaPrice();
 
     }
 }
--- PizzaBillGenerator/src/Pizza.java
+++ PizzaBillGenerator/src/Pizza.java
@@ -1,10 +1,21 @@
 public class Pizza {
-    private String price;
+    private int price;
     private  Boolean veg;
 
     public Pizza(Boolean veg) {
         this.veg = veg;
+        if(this.veg){
+            this.price=300;
+        }
+        else {
+            this.price=400;
+        }
     }
 
-    
+    public  void  getPizzaPrice(){
+        System.out.println(this.price);
+    }
+
+
+
 }
