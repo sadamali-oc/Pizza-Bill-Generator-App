e845cecb7c0da9cf06bb2d97e6d6a069eb32ae1a
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-07-24 10:36:58 +0530

    Update Pizza.java

--- PizzaBillGenerator/src/Pizza.java
+++ PizzaBillGenerator/src/Pizza.java
@@ -2,6 +2,11 @@ public class Pizza {
     private int price;
     private  Boolean veg;
 
+    private  int extraCheesePrice =100;
+    private  int extraToppingsPrice =150;
+
+    private int bagPack =20;
+
     public Pizza(Boolean veg) {
         this.veg = veg;
         if(this.veg){
@@ -12,10 +17,33 @@ public class Pizza {
         }
     }
 
-    public  void  getPizzaPrice(){
-        System.out.println(this.price);
+//    public  void  getPizzaPrice(){
+//        System.out.println(this.price);
+//    }
+
+
+    public  void  addExtraCheese(){
+        System.out.println("Extra cheese added");
+        this.price +=extraCheesePrice;
     }
 
+    public  void  addExtraToppings(){
+        System.out.println("Extra toppings added");
+        this.price +=extraToppingsPrice;
+    }
+
+
+    public  void takeAway(){
+        System.out.println("Take away opted");
+        this.price +=bagPack;
+
+    }
+
+
+    public  void getBill(){
+
+
+    }
 
 
 }
